a9945c35ccdaf3a75153a4a5f6ec70410b7ac338
wanglong1992 <962549004@qq.com>
2018-03-26 16:44:15 +0800

    fun

--- bos_management/src/main/java/cn/itcast/service/PermissionService.java
+++ bos_management/src/main/java/cn/itcast/service/PermissionService.java
@@ -1,4 +1,9 @@
 package cn.itcast.service;
 
+import cn.itcast.domain.system.Permission;
+
+import java.util.List;
+
 public interface PermissionService {
+    List<Permission> findAll();
 }
--- bos_management/src/main/java/cn/itcast/service/impl/PermissionServiceImpl.java
+++ bos_management/src/main/java/cn/itcast/service/impl/PermissionServiceImpl.java
@@ -1,8 +1,20 @@
 package cn.itcast.service.impl;
 
+import cn.itcast.dao.PermissionRepository;
+import cn.itcast.domain.system.Permission;
 import cn.itcast.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PermissionServiceImpl implements PermissionService {
+    @Autowired
+    private PermissionRepository permissionRepository;
+
+    @Override
+    public List<Permission> findAll() {
+        return permissionRepository.findAll();
+    }
 }
