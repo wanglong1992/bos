2cf06980df19427b5de1fe6871361cc12efe642d
wanglong1992 <962549004@qq.com>
2018-03-28 17:35:26 +0800

    fun
    (cherry picked from commit 6d8a67b5d86a403523b8a5949705755cf3212582)

---
+++ bos_management/src/main/java/cn/itcast/index/WayBillIndexRepository.java
@@ -0,0 +1,11 @@
+package cn.itcast.index;
+
+import cn.itcast.domain.take_delivery.WayBill;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
+
+public interface WayBillIndexRepository extends ElasticsearchRepository<WayBill, Integer> {
+
+    public List<WayBill> findByWayBillNum(String wayBillNum);
+}
--- bos_management/src/main/java/cn/itcast/service/impl/WayBillServiceImpl.java
+++ bos_management/src/main/java/cn/itcast/service/impl/WayBillServiceImpl.java
@@ -2,6 +2,7 @@ package cn.itcast.service.impl;
 
 import cn.itcast.dao.WayBillRepository;
 import cn.itcast.domain.take_delivery.WayBill;
+import cn.itcast.index.WayBillIndexRepository;
 import cn.itcast.service.WayBillService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,13 +13,17 @@ import org.springframework.stereotype.Service;
 public class WayBillServiceImpl implements WayBillService {
     @Autowired
     private WayBillRepository wayBillRepository;
+    @Autowired
+    private WayBillIndexRepository wayBillIndexRepository;
+
     @Override
     public void save(WayBill wayBill) {
         wayBillRepository.save(wayBill);
     }
 
     @Override
-    public Page<WayBill> pageQuery(Pageable pageable) {
+    public Page<WayBill> pageQuery(Pageable pageable, WayBill wayBill) {
+
         return wayBillRepository.findAll(pageable);
     }
 }
