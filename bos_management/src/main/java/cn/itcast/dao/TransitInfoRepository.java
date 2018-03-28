d6c41a6dc6da4bac4f8914ddd622e941fa1ea303
wanglong1992 <Java921105>
2018-03-29 01:01:58 +0800

    cc

--- bos_management/src/main/java/cn/itcast/dao/TransitInfoRepository.java
+++ bos_management/src/main/java/cn/itcast/dao/TransitInfoRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface TransitInfoRepository extends JpaRepository<TransitInfo, Integer>, JpaSpecificationExecutor<TransitInfo> {
+
 }
