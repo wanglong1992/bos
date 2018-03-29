03d46bebb9cf0f0bb2733c175859e0a2812b6e7f
wanglong1992 <962549004@qq.com>
2018-03-29 21:39:38 +0800

    fun
    (cherry picked from commit 6d8a67b5d86a403523b8a5949705755cf3212582)

--- bos_domain/src/main/java/cn/itcast/domain/constants/Constants.java
+++ bos_domain/src/main/java/cn/itcast/domain/constants/Constants.java
@@ -5,6 +5,7 @@ package cn.itcast.domain.constants;
  */
 public class Constants {
 
+
     public static final String BOS_MANAGEMENT_HOST = "http://localhost:8080";
     public static final String CRM_MANAGEMENT_HOST = "http://localhost:9002";
     public static final String BOS_FORE_HOST = "http://localhost:9003";
